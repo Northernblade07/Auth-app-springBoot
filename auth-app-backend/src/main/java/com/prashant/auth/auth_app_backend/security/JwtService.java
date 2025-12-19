ea00e8dade28dcd23a3b9eb9d97ce77da8c9e665
Prashannt Bhandari <prashantbhandaricr07@gmail.com>
2025-12-19 16:46:05 +0530

    implement JwtServices

---
+++ auth-app-backend/src/main/java/com/prashant/auth/auth_app_backend/security/JwtService.java
@@ -0,0 +1,113 @@
+package com.prashant.auth.auth_app_backend.security;
+
+import com.prashant.auth.auth_app_backend.entities.Role;
+import com.prashant.auth.auth_app_backend.entities.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+
+//    here we have to opertaions related to jwt
+    private final SecretKey key;
+    private final long accessTtlSeconds;
+    private final long refreshTtlSeconds;
+    private final String issuer;
+
+    public JwtService(
+                    @Value("${security.jwt.secret}") String secret,
+                    @Value("${security.jwt.access-ttl-seconds}") long accessTtlSeconds,
+                    @Value("${security.jwt.refresh-ttl-seconds}") long refreshTtlSeconds,
+                    @Value("${security.jwt.issuer}") String issuer){
+
+        if(secret==null || secret.length()<64){
+            throw new IllegalArgumentException("Invalid secret");
+        }
+
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.accessTtlSeconds = accessTtlSeconds;
+        this.refreshTtlSeconds = refreshTtlSeconds;
+        this.issuer = issuer;
+    }
+
+
+//    generate Access tokens
+    public String generateAccessToken(User user){
+        Instant now  = Instant.now();
+        List<String> roles = user.getRoles()==null? List.of():
+                user.getRoles().stream().map(Role::getName).toList();
+        return Jwts.builder()
+                .id(UUID.randomUUID().toString())
+                .subject(user.getId().toString())
+                .issuer(issuer)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusSeconds(accessTtlSeconds)))
+                .claims(Map.of(
+                        "email",user.getEmail(),
+                        "roles",roles,
+                        "typ","access"
+                ))
+                .signWith(key, SignatureAlgorithm.HS512 )
+                .compact();
+    }
+
+//    generate Refresh Token
+
+    public String generateRefreshToken(User user , String jti){
+        Instant now = Instant.now();
+        return Jwts.builder()
+                .id(jti)
+                .subject(user.getId().toString())
+                .issuer(issuer)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusSeconds(refreshTtlSeconds)))
+                .claim("typ", "refresh")
+                .signWith(key,SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+//    parse token
+
+    public Jws<Claims> parse(String token){
+        try {
+            return Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+        } catch (JwtException e){
+            throw e;
+        }
+    }
+
+//    isAccessToken
+    public boolean isAccessToken(String token){
+        Claims c = parse(token).getPayload();
+        return "access".equals(c.get("typ"));
+    }
+
+//    isRefreshToken
+    public boolean isRefreshToken(String token){
+        Claims c = parse(token).getPayload();
+        return "refresh".equals(c.get("typ"));
+    }
+
+//    get userID from token
+    public UUID getUserIdByToken(String token){
+        Claims c = parse(token).getPayload();
+        return UUID.fromString(c.getSubject());
+    }
+
+//    get tokenId
+
+    public String getTokenId(String token){
+        Claims c = parse(token).getPayload();
+        return c.getId();
+    }
+}
