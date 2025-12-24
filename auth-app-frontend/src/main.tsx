83575011f56cc501154464a3f0295aabad3e982e
Prashannt Bhandari <prashantbhandaricr07@gmail.com>
2025-12-24 18:13:32 +0530

    setup tailwindCss , shadCn and routes

--- auth-app-frontend/src/main.tsx
+++ auth-app-frontend/src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import {BrowserRouter, Route, Routes} from "react-router"
+import { About } from './pages/About.tsx'
+import { Login } from './pages/Login.tsx'
+import { SignUp } from './pages/SignUp.tsx'
+import { Services } from './pages/Services.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+   <Route path='/' element={<App />}/> 
+   <Route path='/about' element={<About />}/> 
+   <Route path='/login' element={<Login />}/>
+   <Route path='/signUp' element={<SignUp />}/> 
+<Route path='/services' element={<Services />}/> 
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
